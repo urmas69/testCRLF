493f0f917414ff1ef5cc54081f9d5772a967807c
urmas69 <urmas.ala@redmedical.de>
2014-03-12 14:59:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,18 @@ NODE_VERSION=v0.10.2
 MONGODB_VERSION=2.4.9
 REDIS_VERSION=2.8.7
 
+run(){
+  echo "run:"
+  $0
+}
+
 CORES=`grep -c processor /proc/cpuinfo`
 
 read -r -n1 -p "Install node ? [y/N]" install_node_yes
 
 echo  "Install node ? $install_node_yes"
 
+run 'ls -la /etc'
 
 su red <<EOSU
 cd
