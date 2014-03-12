74edf5dd3ab0ae27e3a0443630da167da5d3e6fc
Urmas Ala <urmas.ala@redmedical.de>
2014-03-12 15:32:25 +0100

    4

--- install.sh
+++ install.sh
@@ -42,24 +42,8 @@ install_node() {
 
        wrap "install prerequists" apt-get install python g++ make
        echo
-
-       return
        mkdir ~/nodejs && cd $_
-       wget -N http://nodejs.org/dist/node-latest.tar.gz
-       #bar -n  node-latest.tar.gz | tar xzf -
-       progress "unpack" & && MYSELF=$!
-       tar xzf node-latest.tar.gz >/dev/null 2>&1
-       kill $MYSELF >/dev/null 2>&1 && echo -n "...done."
-       cd `ls -d --color=never node-v*`
-       progress "configure node" & && MYSELF=$!
-       ./configure  2>&1 >/dev/null
-       #./configure |xargs -L 2 |xargs -I@ echo -n "." && echo
-       echo "make node"
-       make -j$CORES install 2>&1 >/dev/null | bar -n
-       #make -j$CORES install #|xargs -L 10 |xargs -I@ echo -n "." && echo
-       echo "Installed version of $node is $($node --version)"
     fi
-
 }
 
 CORES=`grep -c processor /proc/cpuinfo`
