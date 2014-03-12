0f9be8387c9b2763dfa6de14c8dac11904cddaac
urmas69 <urmas.ala@redmedical.de>
2014-03-12 15:34:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NODE_VERSION=0.10.2
+NODE_VERSION=0.10.27
 MONGODB_VERSION=2.4.9
 REDIS_VERSION=2.8.7
 
@@ -33,7 +33,7 @@ install_node() {
     node_installed_version=""
     if [ ! -z $(command -v node) ] ;then
         node="$(which node)"
-        node_installed_version=$($node --version)
+        node_installed_version=$($node --version | tr -d "[v,]")
     fi
 
     echo "Installed version of $node is $node_installed_version (expected $NODE_VERSION)"
@@ -42,8 +42,24 @@ install_node() {
 
        wrap "install prerequists" apt-get install python g++ make
        echo
+
+       return
        mkdir ~/nodejs && cd $_
+       wget -N http://nodejs.org/dist/node-latest.tar.gz
+       #bar -n  node-latest.tar.gz | tar xzf -
+       progress "unpack" & && MYSELF=$!
+       tar xzf node-latest.tar.gz >/dev/null 2>&1
+       kill $MYSELF >/dev/null 2>&1 && echo -n "...done."
+       cd `ls -d --color=never node-v*`
+       progress "configure node" & && MYSELF=$!
+       ./configure  2>&1 >/dev/null
+       #./configure |xargs -L 2 |xargs -I@ echo -n "." && echo
+       echo "make node"
+       make -j$CORES install 2>&1 >/dev/null | bar -n
+       #make -j$CORES install #|xargs -L 10 |xargs -I@ echo -n "." && echo
+       echo "Installed version of $node is $($node --version)"
     fi
+
 }
 
 CORES=`grep -c processor /proc/cpuinfo`
