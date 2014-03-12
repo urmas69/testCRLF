f5ee2361c7618b9f42401054f08839b6392d4cca
urmas69 <urmas.ala@redmedical.de>
2014-03-12 14:36:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ read -r -n1 -p "Install node ? [y/N]" install_node_yes
 echo  "Install node ? $install_node_yes"
 
 
-su red -c source<(
-<<EOSU
+source <<EOSU
 cd
 pwd
 whoami
@@ -25,7 +24,7 @@ echo
 
 
 EOSU
-)
+
 
 
 
